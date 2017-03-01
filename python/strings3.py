29b5a5cb0b2e6a28076628094184b38224b82c24
user01 <user01@teaching-98C5.arran>
2017-03-01 16:07:12 +0000

    adding files to github

---
+++ python/strings3.py
@@ -0,0 +1,14 @@
+something = "Completely Different"
+print dir(something)
+
+something.count("t")
+something.find("plete")
+
+something.split("e")
+thing2 = something.replace("Different", "Silly")
+
+print thing2
+
+something[0] = "B"
+
+
